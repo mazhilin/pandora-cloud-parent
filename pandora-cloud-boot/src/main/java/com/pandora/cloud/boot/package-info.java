09d31cf7ad9fd443aee7d71ee8b5e3cb373b11df
marklin <2273990296@qq.com>
2023-05-24 01:15:24 +0800

    [master 2022-05-23 星期三 00:04]:提交parent模块规划

---
+++ pandora-cloud-boot/src/main/java/com/pandora/cloud/boot/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * @className: com.pandora.cloud.boot.package-info
+ * @title: 封装pandoraCloud项目-package-info类
+ * @description: <p>
+ *         pandoraCloud项目-
+ *         </p>
+ * @content: boot
+ * @author: Powered by marklin
+ * @datetime: 2023-05-24 00:09
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 pandoraCloud Systems Incorporated. All rights reserved.
+ */
+package com.pandora.cloud.boot;
