1efe5ee6ec8d9b23b1d52b26be9b8e791189fa78
marklin <2273990296@qq.com>
2023-05-24 00:12:19 +0800

    [master 2022-05-23 星期三 00:04]:提交parent模块规划

---
+++ pandroa-cloud-boot/src/main/java/com/pandroa/cloud/boot/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * @className: com.pandroa.cloud.boot.package-info
+ * @title: 封装PandroaCloud项目-package-info类
+ * @description: <p>
+ *         PandroaCloud项目-
+ *         </p>
+ * @content: boot
+ * @author: Powered by marklin
+ * @datetime: 2023-05-24 00:09
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 PandroaCloud Systems Incorporated. All rights reserved.
+ */
+package com.pandroa.cloud.boot;
